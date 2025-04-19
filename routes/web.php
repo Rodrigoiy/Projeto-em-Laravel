ece947eec0c63396814709c5fe1ddb137213ed36
Rodrigoiy <rodrigoissamuyakushijin@gmail.com>
2025-04-19 12:14:15 -0300

    implementa importação de usuários via CSV com verificação de e-mail e senha aleatória

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+route::get('/', [UserController::class, 'index'])->name('user.index');
+route::post('users-import', [UserController::class, 'import'])->name('user.import');
